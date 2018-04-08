a09c02ef38537436dfcc62877629ec6b1c212030
Chris Wilding <hello@chriswilding.co.uk>
2018-04-08 13:28:32 +0100

    Add jest

---
+++ test/components/App.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import App from '../../src/components/App';
+
+describe('App', () => {
+  it('displays hello world', () => {
+    const component = shallow(<App />);
+    expect(component.text()).toEqual('Hello World!');
+  });
+});
