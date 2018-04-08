8371eb1d673b7fcff8317a2bbd30d5b7f96a3452
Chris Wilding <hello@chriswilding.co.uk>
2018-04-08 13:20:28 +0100

    Add Babel, React and Webpack

---
+++ webpack.config.babel.js
@@ -0,0 +1,40 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from 'path';
+
+export default {
+  entry: './src/index.jsx',
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        exclude: /node_modules/,
+        test: /\.(js|jsx)$/,
+        use: {
+          loader: 'babel-loader',
+        }
+      }
+    ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      inject: 'body',
+      template: './public/index.html'
+    })
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+}
