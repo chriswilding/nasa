14f19e7254d80bbc6caccab8f98c6130dc7d0f29
Chris Wilding <hello@chriswilding.co.uk>
2018-04-28 14:43:52 +0100

    Add docker deploy script

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/sh -e
+docker build -t cw-nasa .;
+echo "$DOCKER_PASSWORD" | docker login --username="$DOCKER_USERNAME" --password-stdin registry.heroku.com;
+docker tag cw-nasa registry.heroku.com/cw-nasa/web;
+docker push registry.heroku.com/cw-nasa/web;
